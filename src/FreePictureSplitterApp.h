7f2383f19028e088315a7177ad6cc57c438f7deb
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-01-29 22:29:40 +0800

    Create FreePictureSplitterApp.h

---
+++ src/FreePictureSplitterApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      FreePictureSplitterApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Grit Clef (3396563372@qq.com)
+ * Created:   2024-01-25
+ * Copyright: Grit Clef (zxunge.github.io)
+ * License:
+ **************************************************************/
+
+#ifndef FREEPICTURESPLITTERAPP_H
+#define FREEPICTURESPLITTERAPP_H
+
+#include <wx/app.h>
+
+class FreePictureSplitterApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // FREEPICTURESPLITTERAPP_H
