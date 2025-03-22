793641c0b65d6523f0f9f320f329b1228dac0eb5
zxunge <3396563372@qq.com>
2025-03-22 21:21:30 +0800

    Update SPDX License Identifier.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-//# This file is a part of FreePictureSplitter and is subject to the the terms of the GPLv3 license.
+//# This file is a part of FreePictureSplitter and is subject to the the terms of the GPL-3.0 license.
 //# Copyright (c) zxunge <3396563372@qq.com>
 //# See https://github.com/zxunge/FreePictureSplitter/blob/main/LICENSE for the full license text.
-// SPDX-License-Identifier: GPLv3
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 
 #include "fpsmainwindow.h"
