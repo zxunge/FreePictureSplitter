ff830b7e74b559cf2d926d1d56b40d49ebcaad22
zxunge <3396563372@qq.com>
2025-04-30 19:35:05 +0800

    Add ribbon ctrl.

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,14 @@
 //# See https://github.com/zxunge/FreePictureSplitter/blob/main/LICENSE for the full license text.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-
 #include "fpsmainwindow.h"
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-#include "qjsonconfig.h"
 
-QJSONConfig cfg("conf.json");
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_WIN_DEBUG_CONSOLE", "attach");
-    qputenv("QSG_INFO", "1");
-
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
         Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
@@ -38,18 +32,9 @@ int main(int argc, char *argv[])
         }
     }
 
-    // First time?
-    /* if (cfg.empty())
-    {
-        cfg.setValue("app/name", "FreePictureSplitter");
-        cfg.setValue("app/author", "zxunge (Grit Clef)");
-        cfg.setValue("style/isDark", true);
-    } */
-
     fpsMainWindow w;
     w.show();
 
     int ret = a.exec();
-    cfg.sync();
     return ret;
 }
