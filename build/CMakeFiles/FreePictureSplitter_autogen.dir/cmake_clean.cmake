f4e967ee85450ce1ec6ea3c351b89d3755dd2fc5
Automated Publisher <actions@users.noreply.github.com>
2025-05-18 03:13:42 +0000

    Automated publish: Sun May 18 03:13:42 UTC 2025 02153eb62c662a694b1b99a2d6e7c98c06b4505d

---
+++ build/CMakeFiles/FreePictureSplitter_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FreePictureSplitter_autogen"
+  "FreePictureSplitter_autogen/include/src/ui/ui_fpsbatchdialog.h"
+  "FreePictureSplitter_autogen/include/src/ui/ui_fpsmainwindow.h"
+  "FreePictureSplitter_autogen/include/src/ui/ui_fpsprogressdialog.h"
+  "FreePictureSplitter_autogen/include/src/ui/ui_fpssettingsdialog.h"
+  "FreePictureSplitter_autogen/mocs_compilation.cpp"
+  "FreePictureSplitter_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FreePictureSplitter_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
