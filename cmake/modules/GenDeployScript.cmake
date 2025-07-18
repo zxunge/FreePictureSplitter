40e4fae878975baf4ca614e82d5f5d9cd76a7ce9
zxunge <3396563372@qq.com>
2025-07-18 14:18:28 +0000

    Deployment: Now the problems are completely fixed.

--- cmake/modules/GenDeployScript.cmake
+++ cmake/modules/GenDeployScript.cmake
@@ -5,27 +5,40 @@
 
 # Custom deployment script generation
 function(gen_deploy_script)
-    set(one_value_args TARGET SCRIPT WINDEPLOY)
-    cmake_parse_arguments(PARSE_ARGV 0 arg "${options}" "${one_value_args}" "")
-    # Project-specific content + Qt's generation
+    set(one_value_args TARGET OUTPUT_SCRIPT WINDEPLOY)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "${one_value_args}" "")
+    set(deploy_script "${CMAKE_CURRENT_BINARY_DIR}/deploy_${arg_TARGET}.cmake")
+    # Project-specific content & Qt's generation
     if(${arg_WINDEPLOY})
-        set(header "
-            set(QT_DEPLOY_PLUGINS_DIR \"plugins\")
-            set(QT_DEPLOY_TRANSLATIONS_DIR \"translations\")
+        set(content "
+set(QT_DEPLOY_PLUGINS_DIR \"plugins\")
+set(QT_DEPLOY_TRANSLATIONS_DIR \"translations\")
+
+include(\"${QT_DEPLOY_SUPPORT}\")
+
+qt_deploy_runtime_dependencies(
+    EXECUTABLE \"$<TARGET_FILE_NAME:${arg_TARGET}>\"
+    GENERATE_QT_CONF
+    NO_TRANSLATIONS
+)
+qt_deploy_translations(CATALOGS \"qtbase\")    ## It seems a bug that Qt cannot infer the catalogs automatically, so we need to specify it manually.
         ")
     else()
-        set(header "
-            set(QT_DEPLOY_PLUGINS_DIR \"${CMAKE_INSTALL_LIBDIR}/plugins\")
-            set(QT_DEPLOY_TRANSLATIONS_DIR \"${CMAKE_INSTALL_DATADIR}/fps/translations\")
+        set(content "
+set(QT_DEPLOY_PLUGINS_DIR \"${CMAKE_INSTALL_LIBDIR}/plugins\")
+set(QT_DEPLOY_TRANSLATIONS_DIR \"${CMAKE_INSTALL_DATADIR}/fps/translations\")
+
+include(\"${QT_DEPLOY_SUPPORT}\")
+
+qt_deploy_runtime_dependencies(
+    EXECUTABLE \"\${QT_DEPLOY_BIN_DIR}/$<TARGET_FILE_NAME:${arg_TARGET}>\"
+    GENERATE_QT_CONF
+    NO_TRANSLATIONS
+)
+qt_deploy_translations(CATALOGS \"qtbase\")
         ")
     endif()
     
-    qt_generate_deploy_app_script(
-        TARGET ${arg_TARGET}
-        OUTPUT_SCRIPT deploy_script
-    )
-    
-    file(READ ${deploy_script} origin)
-    string(APPEND header ${origin})
-    file(WRITE ${arg_SCRIPT} ${header})
+    file(GENERATE OUTPUT ${deploy_script} CONTENT ${content})
+    set(${arg_OUTPUT_SCRIPT} ${deploy_script} PARENT_SCOPE)
 endfunction()
