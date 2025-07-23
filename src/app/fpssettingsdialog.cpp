602b75e80c8fbef1f175ea592bc5ce8cc23e05e0
zxunge <3396563372@qq.com>
2025-07-23 08:53:07 +0000

    License Header: Update.

--- src/app/fpssettingsdialog.cpp
+++ src/app/fpssettingsdialog.cpp
@@ -1,6 +1,20 @@
-// # This file is a part of FreePictureSplitter and is subject to the the terms of the GPL-3.0 license.
-// # Copyright (c) 2024 2025 zxunge
-// # See https://github.com/zxunge/FreePictureSplitter/blob/main/LICENSE for the full license text.
+/*
+ * This file is a part of FreePictureSplitter, a fast and easy-to-use picture splitter.
+ * Copyright (c) 2024 2025 zxunge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "fpssettingsdialog.h"
