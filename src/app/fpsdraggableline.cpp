dbaf5de0b32397db2fa02887d6584a8a0b2a119f
zxunge <3396563372@qq.com>
2025-07-25 13:34:56 +0000

    Add files which were not added the last commit.

---
+++ src/app/fpsdraggableline.cpp
@@ -0,0 +1,152 @@
+/*
+ * This file is a part of FreePictureSplitter, a fast and easy-to-use picture splitter.
+ * Copyright (c) 2024 2025 zxunge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "fpsdraggableline.h"
+
+#include <QSizePolicy>
+#include <QMouseEvent>
+#include <QCursor>
+#include <QGraphicsView>
+#include <QColor>
+#include <QPalette>
+
+using namespace Qt::Literals::StringLiterals;
+
+fpsDraggableLine::fpsDraggableLine(QGraphicsView *parent, Qt::Orientation orientation,
+                                   const QPoint &pos)
+    : QPushButton(parent)
+{
+    setAutoFillBackground(true);
+    setPalette(QPalette(QColor(50, 205, 50)));
+
+    m_orientation = orientation;
+    if (m_orientation == Qt::Horizontal) {
+        setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
+        resize(parent->width(), LINE_SIZE);
+        move(0, pos.y());
+        m_scenePos = parent->mapToScene(parent->viewport()->mapFrom(parent, pos)).toPoint().y();
+    } else {
+        setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding));
+        resize(LINE_SIZE, parent->height());
+        move(pos.x(), 0);
+        m_scenePos = parent->mapToScene(parent->viewport()->mapFrom(parent, pos)).toPoint().x();
+    }
+    raise(); // Move to top
+}
+
+void fpsDraggableLine::updateLine(const QPoint &pos)
+{
+    QGraphicsView *parent{ qobject_cast<QGraphicsView *>(parentWidget()) };
+    if (m_orientation == Qt::Horizontal) {
+        resize(parentWidget()->width(), LINE_SIZE);
+        m_scenePos = parent->mapToScene(parent->viewport()->mapFrom(parent, pos)).toPoint().y();
+        move(0, pos.y());
+    } else {
+        resize(LINE_SIZE, parentWidget()->height());
+        m_scenePos = parent->mapToScene(parent->viewport()->mapFrom(parent, pos)).toPoint().x();
+        move(pos.x(), 0);
+    }
+}
+
+void fpsDraggableLine::updateLine()
+{
+    QGraphicsView *parent{ qobject_cast<QGraphicsView *>(parentWidget()) };
+    // Moving according to the stored scenePos
+    if (m_orientation == Qt::Horizontal) {
+        resize(parentWidget()->width(), LINE_SIZE);
+        move(0, parent->viewport()->mapTo(parent, parent->mapFromScene(0, m_scenePos)).y());
+    } else {
+        resize(LINE_SIZE, parentWidget()->height());
+        move(parent->viewport()->mapTo(parent, parent->mapFromScene(m_scenePos, 0)).x(), 0);
+    }
+}
+
+void fpsDraggableLine::setScenePos(int pos)
+{
+    m_scenePos = pos;
+    updateLine();
+}
+
+void fpsDraggableLine::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+        m_pressed = true;
+    QPushButton::mousePressEvent(event);
+}
+
+void fpsDraggableLine::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() == Qt::LeftButton) {
+        QGraphicsView *parent{ qobject_cast<QGraphicsView *>(parentWidget()) };
+        if (m_pressed && m_orientation == Qt::Horizontal) {
+            move(0, mapToParent(event->pos()).y());
+            m_scenePos =
+                    parent->mapToScene(parent->viewport()->mapFromParent(mapToParent(event->pos())))
+                            .y();
+        } else if (m_pressed && m_orientation == Qt::Vertical) {
+            move(mapToParent(event->pos()).x(), 0);
+            m_scenePos =
+                    parent->mapToScene(parent->viewport()->mapFromParent(mapToParent(event->pos())))
+                            .x();
+        }
+    }
+    QPushButton::mouseMoveEvent(event);
+}
+
+void fpsDraggableLine::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        QGraphicsView *parent{ qobject_cast<QGraphicsView *>(parentWidget()) };
+        if (m_pressed && m_orientation == Qt::Horizontal) {
+            m_scenePos =
+                    parent->mapToScene(parent->viewport()->mapFromParent(mapToParent(event->pos())))
+                            .y();
+            if (m_scenePos >= parent->scene()->height() || m_scenePos <= 0)
+                Q_EMIT userDestruction();
+            move(0, mapToParent(event->pos()).y());
+            m_pressed = false;
+        } else if (m_pressed && m_orientation == Qt::Vertical) {
+            m_scenePos =
+                    parent->mapToScene(parent->viewport()->mapFromParent(mapToParent(event->pos())))
+                            .x();
+            if (m_scenePos >= parent->scene()->width() || m_scenePos <= 0)
+                Q_EMIT userDestruction();
+            move(mapToParent(event->pos()).x(), 0);
+            m_pressed = false;
+        }
+    }
+    QPushButton::mouseReleaseEvent(event);
+}
+
+bool fpsDraggableLine::event(QEvent *event)
+{
+    switch (event->type()) {
+    case QEvent::Enter:
+        setStyleSheet(u"background-color: rgba(50, 205, 50, 0.5);"_s); // Highlighting
+        setCursor(QCursor(m_orientation == Qt::Horizontal ? Qt::SizeVerCursor : Qt::SizeHorCursor));
+        break;
+    case QEvent::Leave:
+        setCursor(QCursor(Qt::ArrowCursor));
+        setStyleSheet(u"background-color: rgb(50, 205, 50);"_s);
+        break;
+    default:
+        break;
+    }
+    return QPushButton::event(event);
+}
---
+++ src/app/fpsdraggableline.h
@@ -0,0 +1,77 @@
+/*
+ * This file is a part of FreePictureSplitter, a fast and easy-to-use picture splitter.
+ * Copyright (c) 2024 2025 zxunge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// SPDX-License-Identifier: GPL-3.0-or-later
+// clang-format Language: Cpp
+
+#ifndef FPSDRAGGABLELINE_H
+#define FPSDRAGGABLELINE_H
+
+#include "fpsrulerbar.h"
+#include <QPushButton>
+
+class QMouseEvent;
+class QEvent;
+class QGraphicsView;
+
+constexpr int LINE_SIZE{ 2 };
+
+class fpsDraggableLine : public QPushButton
+{
+    Q_OBJECT
+public:
+    // According to Qt documentation, the following 2 'pos'es
+    // refers to the position in whole GraphicsView.
+    explicit fpsDraggableLine(QGraphicsView *parent = nullptr,
+                              Qt::Orientation orientation = Qt::Horizontal,
+                              const QPoint &pos = QPoint(0, RULER_SIZE));
+
+    void updateLine(const QPoint &pos); // Move on GraphicsView and update scenePos; pos -> whole
+                                        // GraphicsView
+
+    void updateLine(int x, int y) { updateLine(QPoint(x, y)); } // Overloaded for convenience
+
+    void updateLine(); // Update itself through existing scenePos
+
+    int scenePos() const { return m_scenePos; }
+    // We assume that you specify the correct value for different directions. pos -> scene
+    void setScenePos(int pos);
+
+    Qt::Orientation getOrientation() const { return m_orientation; }
+
+    // An identifier, enabling us to find it if it is being destroyed.
+    void setId(quint64 id) { m_id = id; }
+    quint64 id() const { return m_id; }
+
+signals:
+    void userDestruction();
+
+protected:
+    // event->pos() -> fpsDraggableLine itself
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    bool event(QEvent *event) override;
+
+private:
+    bool m_pressed{ false };
+    Qt::Orientation m_orientation;
+    int m_scenePos; // Position in a scene (V:x or H:y)
+    quint64 m_id; // An identifier, enabling us to find it.
+};
+
+#endif // FPSDRAGGABLELINE_H
