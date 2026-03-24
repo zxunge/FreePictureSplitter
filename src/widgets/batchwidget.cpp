95b7385e8cade36399f9cf24c94853f95b62e5b4
zxunge <3396563372@qq.com>
2026-03-24 11:52:13 +0800

    Reserve some features for release.

--- src/widgets/batchwidget.cpp
+++ src/widgets/batchwidget.cpp
@@ -55,6 +55,10 @@ BatchWidget::BatchWidget(QWidget *parent)
 {
     ui->setupUi(this);
 
+     // Reserved features
+    ui->rbtnTemplate->setVisible(false);
+    ui->cbxTemplate->setVisible(false);
+
     ui->viewTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Interactive);
 
     QActionGroup *ag = new QActionGroup(this);
